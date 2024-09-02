64b6f316cc88872d84fa82d07847612a24fa3014
KevinG <amirprogcode@gmail.com>
2024-09-02 22:54:30 +0330

    Tamrin4 ==> FizzBuzz

--- 04-FizzBuzz/fizzBuzz.js
+++ 04-FizzBuzz/fizzBuzz.js
@@ -1,1 +1,13 @@
 // write your func here
+
+for(let item = 1; item <= 100 ; item++){
+    if(item % 3 === 0 && item % 5 === 0){
+        console.log("FizzBuzz");
+    }
+    else if(item % 3 === 0){
+        console.log("Fizz");
+    }
+    else if(item % 5 === 0){
+        console.log("Buzz");
+    }else console.log(item);
+};
