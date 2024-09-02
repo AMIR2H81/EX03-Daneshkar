0761c90aeff5b54df7499828087f8cd114842328
KevinG <amirprogcode@gmail.com>
2024-09-02 22:53:25 +0330

    Tamrin2 ==> palondrom

--- 02-plaindrom/plaidrom.js
+++ 02-plaindrom/plaidrom.js
@@ -1,1 +1,11 @@
 // write your func here
+
+function palindrome(str){
+    let lowStr = str.toLowerCase();
+    let reverseStr = lowStr.split("").reverse().join("");
+    if(reverseStr === lowStr){
+        return true;
+    }else return false;
+};
+
+console.log(palindrome("amir"));
