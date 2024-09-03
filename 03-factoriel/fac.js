c9fa36d2484c7c0f05fe80859f34b3569b19891e
KevinG <amirprogcode@gmail.com>
2024-09-03 23:14:18 +0330

    Tamrin3 ==> factoriel

--- 03-factoriel/fac.js
+++ 03-factoriel/fac.js
@@ -1,1 +1,18 @@
 // write your func here
+
+function factorial(num){
+    let result = num;
+    if(num === 0 || num === 1){
+        return 1;
+    }
+    else if(num > 1){
+        while(num > 1){
+            num--;
+            result *= num;
+        }
+    }else return "choose a bigger than 0 number please";
+
+    return result;
+};
+
+console.log(factorial(5));
