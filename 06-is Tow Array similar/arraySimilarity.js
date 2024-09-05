14d68160e7c5598881ba618aed1f46701d440519
KevinG <amirprogcode@gmail.com>
2024-09-05 19:09:54 +0330

    Tamrin6 ==> Similar arrays ==> The last exercise / All the exercises were done

--- 06-is Tow Array similar/arraySimilarity.js
+++ 06-is Tow Array similar/arraySimilarity.js
@@ -1,1 +1,18 @@
 // write your func here
+
+function similarArrays(arr1, arr2){
+    if(arr1.length != arr2.length){
+        return false;
+    }else {
+        let result = false;
+
+        for(let item = 0; item < arr1.length; item++){
+            if(arr1[item] !== arr2[item]){
+                return false;
+            } else result = true;
+        };
+        return result;
+    };
+};
+
+console.log(similarArrays([1, 2, 3], [1, "2", 3]));
