5581a7d7c6da8cfade23058da845f35ed79f33b7
KevinG <amirprogcode@gmail.com>
2024-09-06 16:07:15 +0330

    Tamrin6 ==> Edit code

--- 06-is Tow Array similar/arraySimilarity.js
+++ 06-is Tow Array similar/arraySimilarity.js
@@ -4,15 +4,15 @@ function similarArrays(arr1, arr2){
     if(arr1.length != arr2.length){
         return false;
     }else {
-        let result = false;
+        let result = true;
 
         for(let item = 0; item < arr1.length; item++){
             if(arr1[item] !== arr2[item]){
                 return false;
-            } else result = true;
+            };
         };
         return result;
     };
 };
 
-console.log(similarArrays([1, 2, 3], [1, "2", 3]));
+console.log(similarArrays([1, 2, 3], [1, 2, 3]));
